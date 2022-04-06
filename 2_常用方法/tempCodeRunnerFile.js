b9600bc0bf7618b481bd80b5d4f1a2d951e44c16
mirrorgo <cloudmirrorgo@163.com>
2022-04-06 14:43:52 +0800

    1

--- 2_常用方法/tempCodeRunnerFile.js
+++
@@ -1,4 +0,0 @@
-const myType = function (type) {
-    return Object.prototype.toString.call(type).slice(8, -1);
-  };
-  console.log(myType(new Array(1)));
--- tempCodeRunnerFile.js
+++
@@ -1,4 +0,0 @@
-let i = 1,
-  version1 = "1.01";
-let v1s = +version1.substring(i, version1.indexOf(".", i + 1));
-console.log(v1s);
